ad384e308a682daa42fdd599b3138949861d8566
Michael McNeer <mmike070707@gmail.com>
2026-08-06 05:35:30 -0400

    Fix: Use environment variables for Firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJwSjyg1isbYgWXhQL9H663XeYEGMgQ04",
-  authDomain: "gaisb-ad799.firebaseapp.com",
-  projectId: "gaisb-ad799",
-  storageBucket: "gaisb-ad799.firebasestorage.app",
-  messagingSenderId: "395435864000",
-  appId: "1:395435864000:web:ec3237e9b9a68029056225",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
